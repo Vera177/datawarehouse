587c31a96ae1659ad7a555d9d6664dedf2093971
Vera177 <79235446+Vera177@users.noreply.github.com>
2022-08-17 00:29:04 -0300

    setting up connection

--- index.js
+++ index.js
@@ -10,17 +10,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.Promise = global.Promise;
-const userPasswordDatabase = (config.database.name || config.database.password) && `${config.database.user}:${config.database.password}@` 
-mongoose.connect(`mongodb://${userPasswordDatabase}${config.database.host}:${config.database.port}/${config.database.name}`)
-.then(()  => {
-    app.listen(config.port, () => {
-        console.log(`Server started on http://localhost:${config.port} (${config.env})`);
+const userPasswordDatabase = (config.database.user || config.database.password) && `${config.database.user}:${config.database.password}@`
+mongoose.connect(
+    `mongodb://${userPasswordDatabase}${config.database.host}:${config.database.port}/${config.database.name}`)
+    .then(() => {
+        app.listen(config.port, () => {
+            console.log(`Server started on http://localhost:${config.port} (${config.env})`);
+        });
+    })
+    .catch(err => {
+        console.log(`DB connection Error: ${err.message}`);
     });
-})
-.catch(err => {
-    console.log(`DB connection Error: ${err.message}`);
-});
-
-app.listen(config.port, () => {
-    console.log(`Server started on http://localhost:${config.port} (${config.env})`);
-}); 
