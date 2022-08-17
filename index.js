edb7de57eccf755a6f88d519a3cc286f507ea484
Vera177 <79235446+Vera177@users.noreply.github.com>
2022-08-16 23:36:21 -0300

    seting up DB

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const config = require('./config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+const userPasswordDatabase = (config.database.name || config.database.password) && `${config.database.user}:${config.database.password}@` 
+mongoose.connect(`mongodb://${userPasswordDatabase}${config.database.host}:${config.database.port}/${config.database.name}`)
+.then(()  => {
+    app.listen(config.port, () => {
+        console.log(`Server started on http://localhost:${config.port} (${config.env})`);
+    });
+})
+.catch(err => {
+    console.log(`DB connection Error: ${err.message}`);
+});
+
+app.listen(config.port, () => {
+    console.log(`Server started on http://localhost:${config.port} (${config.env})`);
+}); 
