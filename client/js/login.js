03f86c24ea2108f2fb15649b71ade59110c46cd1
Vera177 <79235446+Vera177@users.noreply.github.com>
2022-10-19 12:40:06 -0300

    minor fix

--- client/js/login.js
+++ client/js/login.js
@@ -4,15 +4,17 @@ const txtUser = document.getElementById("username");
 const txtPassword = document.getElementById("password");
 const btnSubmit = document.getElementById("submit");
 
-btnSubmit.addEventListener("click", (e) => {
-    e.preventDefault();
-    request("/api/login", {
-        method: "POST",
-        body: { email: txtUser.value, password: txtPassword.value },
-    }).then((res) => {
-        localStorage.setItem("token", res.token);
-        window.location.replace('/index.html');
-    }).catch(err => {
-      console.log(err);
+if(btnSubmit){
+    btnSubmit.addEventListener("click", (e) => {
+        e.preventDefault();
+        request("/api/login", {
+            method: "POST",
+            body: { email: txtUser.value, password: txtPassword.value },
+        }).then((res) => {
+            localStorage.setItem("token", res.token);
+            window.location.replace('/index.html');
+        }).catch(err => {
+          console.log(err);
+        });
     });
-});
+}
