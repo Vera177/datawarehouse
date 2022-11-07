2aa1b070b64e97f1957d13dbaced34cff5b0d9e3
Vera177 <79235446+Vera177@users.noreply.github.com>
2022-11-07 00:04:05 -0300

    new schema contacts

---
+++ controllers/Contacts.js
@@ -0,0 +1,85 @@
+const contactModel = require('../models/contacts');
+
+class Contactcontroller {
+
+    static async create(req, res) {
+        const { firstname, lastname, email, password, role } = req.body;
+        try {
+            const roleFound = await roleModel.findOne({ _id: role });
+            if (!roleFound) {
+                return res.status(400).json({
+                    status: 400,
+                    error: 'Role ID not found'
+                });
+            }
+            const userCreated = new userModel({
+                firstname, lastname, email, password, roles_id: role
+            });
+            await userCreated.save();
+            return res.status(201).json({
+                status: 201,
+                message: 'User created'
+            });
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+
+    static async getAll(req, res) {
+        try {
+            const users = await userModel.find().populate('roles_id')
+            const data = users.map(user => {
+                return {
+                    id: user._id,
+                    firstname: user.firstname,
+                    lastname: user.lastname,
+                    email: user.email,
+                    role: {
+                        id: user.roles_id._id,
+                        name: user.roles_id.name
+                    }
+                }
+            });
+            return res.json({
+                status: 200,
+                data
+            })
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body;
+        try {
+            const user = await userModel.findOne({ email }).populate('roles_id');
+            if (!user) {
+                throw { status: 401, message: 'Usuario y/o contraseña invalidos' };
+            }
+            const match = await user.comparePassword(password);
+            if (!match) {
+                throw { status: 401, message: 'Usuario y/o contraseña invalidos' };
+            }
+            if(user.roles_id.name != 'admin'){
+                throw {status: 401, message: "Hello admin"}
+            }
+            const token = jwtHelper.encode({
+                checkUser: {
+                    id: user._id,
+                    role: user.roles_id
+                }
+            });
+            return res.json({
+                status: 200,
+                token: token
+            });            
+        } catch (error) {
+            return res.status(error.status || 500).json({
+                status: error.status || 500,
+                message: error.message || 'Internal server error'
+            });
+        }
+    }
+}
+
+module.exports = Contactcontroller;
---
+++ models/contacts.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    firstname: {type: String, require: true},
+    lastname: {type: String, require: true},
+    email: {type: String, require: true},
+    password: {type: String, require: true},
+    adress: {type: String, require: true},
+    roles_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Roles'},
+    company_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Company'},
+    occupation_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Occupation'},
+    contact_information_id: {type: mongoose.Schema.Types.ObjectId, ref: 'ContactInformation'},
+    interested_id: {type: mongoose.Schema.Types.ObjectId, ref: 'InterestLevel'},
+    cities_id: {type: mongoose.Schema.Types.ObjectId, ref: 'City'}
+});
+
+schema.pre('save', function(next) {
+    if(!this.isModified('password')) {
+        return next();
+    }
+    const salt = bcrypt.genSaltSync(10);
+    this.password = bcrypt.hashSync(this.password, salt);
+    next();
+});
+
+schema.methods.comparePassword = function(plaintext, callback) {
+    return new Promise((resolve, reject) => {
+        return resolve(bcrypt.compareSync(plaintext, this.password));
+    });
+}
+
+module.exports = mongoose.model('Contacts', schema);
