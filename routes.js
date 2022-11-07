d22e5dedc4a74f20057edafbd39765424554772b
Vera177 <79235446+Vera177@users.noreply.github.com>
2022-11-06 23:48:16 -0300

    middleware in getAll

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const jwtMiddleware = require('./middlewares/jwtToken');
 const RolesController = require('./controllers/Roles');
 const Usercontroller = require('./controllers/Users');
 
@@ -9,7 +9,7 @@ router.get('/role', RolesController.getAll);
 
 /* users */
 router.post('/user', Usercontroller.create);
-router.get('/user', Usercontroller.getAll);
+router.get('/user', jwtMiddleware, Usercontroller.getAll);
 router.post('/login', Usercontroller.login);
 
 module.exports = router;
